dd9128da227e37daf095060ad1dee373467ae074
khumanpoudel <khumanpoudel124421@gmail.com>
2026-03-22 09:07:51 +0000

    feat: add zod validation schema for register user

---
+++ src/validators/user.validators.ts
@@ -0,0 +1,24 @@
+import z from "zod";
+
+
+const err = {
+    Empty_fullname: "Fullname shouldn't be empty",
+    Short_fullname: "Fullname should be atleast 3 characters long",
+    Empty_username: "Username shouldn't be empty",
+    Empty_email: "Email shouldn't be empty",
+    Invalid_email: "Provide valid email address",
+    Empty_password: "Password shouldn't be empty",
+    Short_password: "Password should be atleast 6 characters long",
+    Empty_body: "Provide valid body data"
+}
+
+export const registerValidator = z.object({
+    fullname: z.string(err.Empty_fullname).min(3, err.Short_fullname),
+    username: z.string(err.Empty_username),
+    email: z.string(err.Empty_email).email(err.Invalid_email),
+    password: z.string(err.Empty_password).min(6, err.Short_password)
+        .regex(/[A-Z]/, "Must contain uppercase letter")
+        .regex(/[a-z]/, "Must contain lowercase letter")
+        .regex(/[0-9]/, "Must contain a number")
+},err.Empty_body);
+
