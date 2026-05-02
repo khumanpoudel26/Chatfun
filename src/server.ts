ca8c04c62e622f1c859cdc9776c11dc6cfa3426f
khumanpoudel <khumanpoudel124421@gmail.com>
2026-05-02 18:23:13 +0545

    feat: add security middlewares

--- src/server.ts
+++ src/server.ts
@@ -5,16 +5,46 @@ import userRoutes from "./routes/user.routes";
 import errorHandler from "./middlewares/errorHandler";
 import emptyBody from "./middlewares/emptyBody";
 import chatRoutes from "./routes/chat.routes";
-
-
+import { Server } from "socket.io";
+import cors from "cors"
+import helmet from "helmet";
+import morgan from "morgan";
 dotenv.config({})
 
 const app = express();
 const server = http.createServer(app);
-
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:5173",
+        credentials: true
+    }
+});
 app.use(express.json({ limit: '200mb' }));
 app.use(express.urlencoded({ extended: true, limit: '200mb' }));
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}));
+app.use(helmet());
+app.use(morgan("dev"));
 //app.use(emptyBody); // Middleware to check for empty body in POST, PUT, PATCH, DELETE requests
+
+io.on("connection", (socket) => {
+    console.log("User connected:", socket.id);
+    const user_id = socket.handshake.query.user_id;
+
+    socket.on("join_chat", (chat_id) => {
+        socket.join(`${chat_id}`);
+        console.log(`User ${user_id} joined chat ${chat_id}`)
+    });
+
+    socket.on("disconnect", () => {
+        console.log("User disconnected:", user_id);
+    });
+
+});
+
+
 const PORT = process.env.PORT || 8081
 
 app.get('/health', (req, res) => {
@@ -25,7 +55,7 @@ app.get('/health', (req, res) => {
 });
 
 app.use('/api/user', userRoutes);
-app.use('/api/chat',chatRoutes);
+app.use('/api/chat', chatRoutes);
 app.use(errorHandler);
 
 
@@ -34,3 +64,4 @@ server.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
 
+export { io }
